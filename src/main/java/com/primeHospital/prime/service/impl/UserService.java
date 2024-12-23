5f3344bd79f112b92ea6dc15a4e5969b8c2b5d28
ARJUN241678 <ARJUNROCKS152@GMAIL.COM>
2024-12-23 11:31:19 +0530

    implemented spring security

--- src/main/java/com/primeHospital/prime/service/impl/UserService.java
+++ src/main/java/com/primeHospital/prime/service/impl/UserService.java
@@ -1,8 +1,8 @@
 package com.primeHospital.prime.service.impl;
-
 import com.primeHospital.prime.Entity.User;
 import com.primeHospital.prime.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,18 +11,25 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public User registerUser(User user) {
         if (userRepository.findByUsername(user.getUsername()).isPresent()) {
             throw new IllegalArgumentException("Username already exists");
         }
-        // Password encryption can be added here for better security
+        user.setPassword(passwordEncoder.encode(user.getPassword())); // Hash the password
         return userRepository.save(user);
     }
 
     public User loginUser(String username, String password) {
-        return userRepository.findByUsername(username)
-                .filter(user -> user.getPassword().equals(password))
+        User user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new IllegalArgumentException("Invalid username or password"));
+
+        if (!passwordEncoder.matches(password, user.getPassword())) {
+            throw new IllegalArgumentException("Invalid username or password");
+        }
+
+        return user;
     }
 }
-
